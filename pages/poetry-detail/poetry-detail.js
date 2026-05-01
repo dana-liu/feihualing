8d7de3cefb622c8bf521a9dc0c40d9639558ad36
liuww <liuww@knowbox.cn>
2026-05-01 17:51:15 +0800

    feat: 诗词详情页添加关键字高亮和收藏功能
    - 添加收藏按钮（与背诗模式联动）
    - 点击关键字可高亮显示诗句中的该字
    - 再次点击取消高亮

--- pages/poetry-detail/poetry-detail.js
+++ pages/poetry-detail/poetry-detail.js
@@ -3,7 +3,10 @@ const poetry = require('../../utils/poetry.js');
 Page({
   data: {
     poem: null,
-    sentences: []
+    sentences: [],
+    isFavorited: false,
+    highlightKeyword: '',
+    favorites: []
   },
 
   onLoad(options) {
@@ -19,28 +22,75 @@ Page({
       return;
     }
 
+    // 加载收藏
+    const favorites = wx.getStorageSync('favorites') || [];
+    const isFavorited = favorites.includes(poem.id);
+
     // 处理诗句，生成sentences结构
+    const sentences = this.processSentences(poem.content);
+
+    this.setData({
+      poem: poem,
+      sentences: sentences,
+      isFavorited: isFavorited,
+      favorites: favorites
+    });
+  },
+
+  processSentences(content, highlightKeyword = '') {
     const sentences = [];
     let currentSentence = [];
     const punctuation = /[，。！？]/;
 
-    for (let i = 0; i < poem.content.length; i++) {
-      const char = poem.content[i];
+    for (let i = 0; i < content.length; i++) {
+      const char = content[i];
       if (punctuation.test(char)) {
-        currentSentence.push({ char: char, isKeyword: false, isPunctuation: true });
+        currentSentence.push({ char: char, isHighlight: false, isPunctuation: true });
         sentences.push(currentSentence);
         currentSentence = [];
       } else {
-        currentSentence.push({ char: char, isKeyword: false });
+        currentSentence.push({
+          char: char,
+          isHighlight: highlightKeyword ? char === highlightKeyword : false
+        });
       }
     }
     if (currentSentence.length > 0) {
       sentences.push(currentSentence);
     }
+    return sentences;
+  },
+
+  highlightKeyword(e) {
+    const keyword = e.currentTarget.dataset.keyword;
+    const { poem, highlightKeyword } = this.data;
+
+    // 切换高亮
+    const newKeyword = highlightKeyword === keyword ? '' : keyword;
+    const sentences = this.processSentences(poem.content, newKeyword);
 
     this.setData({
-      poem: poem,
+      highlightKeyword: newKeyword,
       sentences: sentences
+    });
+  },
+
+  toggleFavorite() {
+    const { poem, favorites } = this.data;
+
+    const poemId = poem.id;
+    const index = favorites.indexOf(poemId);
+
+    if (index > -1) {
+      favorites.splice(index, 1);
+    } else {
+      favorites.push(poemId);
+    }
+
+    wx.setStorageSync('favorites', favorites);
+    this.setData({
+      favorites: favorites,
+      isFavorited: index === -1
     });
   },
 
